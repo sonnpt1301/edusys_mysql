3e9441e3e8292d34dad31b5f947675ca206a3e89
Sotatek-SonNguyen8 <son.nguyen8@sotatek.com>
2022-07-20 17:03:57 +0700

    Initialize logging

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,17 @@ import { AppModule } from './app/app.module';
 import { appConfig } from './configs/configs.constants';
 import { HttpExceptionFilter } from './shared/filter/http-exception.filter';
 import { SwaggerInitialize } from './shared/swaggers/document';
+import { LoggingInterceptor } from './shared/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   SwaggerInitialize(app);
 
   app.useGlobalFilters(new HttpExceptionFilter());
-
   app.useGlobalPipes(
     new ValidationPipe({ transform: true, forbidNonWhitelisted: true }),
   );
-
+  app.useGlobalInterceptors(new LoggingInterceptor());
   app.enableCors();
 
   const { port } = appConfig;
